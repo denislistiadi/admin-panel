906d89db442449e2ba7bf75db7039fd20360edf2
Denis Listiadi <denislistiadi24@gmail.com>
2023-01-25 21:57:43 +0700

    Update Main Layout

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg'
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
+import MainLayout from './components/Layout/MainLayout'
+import Dashboard from './pages/Dashboard'
+import ForgotPassword from './pages/ForgotPassword'
+// import Dashboard from './pages/Dashboard'
+import Login from './pages/Login'
+import ResetPassword from './pages/ResetPassword'
 
 function App() {
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path='/' element={<Login />} />
+      <Route path='/reset-password' element={<ResetPassword />} />
+      <Route path='/forgot-password' element={<ForgotPassword />} />
+      <Route path='/admin' element={<MainLayout />}>
+        <Route index element={<Dashboard />} />
+      </Route>
+    </Routes>
   )
 }
 
